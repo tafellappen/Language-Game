7de81159035029f00a46a7e81cdfe8cf037a5691
Miranda <msa4295@rit.edu>
2023-03-07 19:25:27 -0500

    Finalized the pattern for adding dialogue and responses

--- LanguageProject/Assets/Scripts/Dialogue/DialoguePath.cs
+++ LanguageProject/Assets/Scripts/Dialogue/DialoguePath.cs
@@ -10,12 +10,18 @@ using UnityEngine;
 public class DialoguePath : MonoBehaviour
 {
     [SerializeField] DialogManager DialogManager;
+    //[SerializeField] private Translator translatorScript;
     Dictionary<string, DialogData> responseMap = new Dictionary<string, DialogData>();
     //idea for friendship values: we can probably map responses to corresponding friendship values as well, depending on how we want them to work...
     List<DialogData> dialogDataList = new List<DialogData>();
 
     enum ResponseKeys
     {
+        //Correct,
+        //Wrong,
+        //Whatever,
+        Me,
+        You,
         Float,
         Predict,
         Fly,
@@ -26,15 +32,31 @@ public class DialoguePath : MonoBehaviour
     private void Awake()
     {
         CreateResponseMap();
-        dialogDataList.Add(new DialogData("Hi! You must be the new student from Earth! My name is Cindy! What are you getting for lunch?", "AlienCharacter"));
-        
-        //DialogData Text1 = new DialogData("What is 2 times 5?");
-        //Text1.SelectList.Add(ResponseKeys.Correct.ToString(), "10");
-        //Text1.SelectList.Add(ResponseKeys.Wrong.ToString(), "7");
-        //Text1.SelectList.Add(ResponseKeys.Whatever.ToString(), "Why should I care?");
-        //dialogDataList.Add(Text1);
+        dialogDataList.Add(new DialogData(" ")); //just a hack to not have the symbols in the way for now, since im not calling translate on anything
+        dialogDataList.Add(new DialogData("Welcome to our class. I’m super excited to meet you, as the first human from earth.", "AlienCharacter"));
+        //dialogDataList[0].Callback = () => { translatorScript.PleaseTranslate = true; };
 
-        //Text1.Callback = () => ShowResponse();
+        DialogData data;
+        data = new DialogData("I’m always interested in earth civilizations, so I have tons of questions about earth!", "AlienCharacter");
+        data.SelectList.Add(ResponseKeys.Me.ToString(), "Of course, go ahead and ask me!");
+        data.SelectList.Add(ResponseKeys.You.ToString(), "I also have many questions about your home planet.");
+        data.Callback = () => ShowResponse();
+        dialogDataList.Add(data);
+
+        data = new DialogData("Is it true that gravity on earth is so strong that everything stays on the ground?");
+        data.SelectList.Add(ResponseKeys.Float.ToString(), "Yes, nothing really floats in the air.");
+        data.SelectList.Add(ResponseKeys.Predict.ToString(), "Yes, it allows us to predict movement and organize our life accordingly.");
+        data.SelectList.Add(ResponseKeys.Fly.ToString(), "Well, birds can fly, and we humans have invented planes to get us up in the air.");
+        data.Callback = () => ShowResponse();
+        dialogDataList.Add(data);
+
+
+        data = new DialogData("Did you get used to life here? How do you like the food on this planet?");
+        data.SelectList.Add(ResponseKeys.LikeFood.ToString(), "I like the food here, quite different from what we had on earth.");
+        data.SelectList.Add(ResponseKeys.DislikeFood.ToString(), " It’s so bad. I miss food from earth.");
+        //data.Callback = SecondResponse;
+        dialogDataList.Add(data);
+
 
         DialogManager.Show(dialogDataList);
 
@@ -46,6 +68,13 @@ public class DialoguePath : MonoBehaviour
         //responseMap.Add(ResponseKeys.Correct.ToString(), new DialogData("You are right."));
         //responseMap.Add(ResponseKeys.Wrong.ToString(), new DialogData("You are wrong."));
         //responseMap.Add(ResponseKeys.Whatever.ToString(), new DialogData("Right. You don't have to get the answer."));
+        responseMap.Add(ResponseKeys.Me.ToString(), new DialogData("you are asked questions"));
+        responseMap.Add(ResponseKeys.You.ToString(), new DialogData("you ask the alien questions"));
+        responseMap.Add(ResponseKeys.LikeFood.ToString(), new DialogData("you like the food"));
+        responseMap.Add(ResponseKeys.DislikeFood.ToString(), new DialogData("you dislike the food"));
+        responseMap.Add(ResponseKeys.Float.ToString(), new DialogData("nothing floats?"));
+        responseMap.Add(ResponseKeys.Predict.ToString(), new DialogData("prediction"));
+        responseMap.Add(ResponseKeys.Fly.ToString(), new DialogData("tobenai tori"));
     }
 
     private void ShowResponse()
